50f061d0fbc4d9055342566273681101fe8fef55
Alireza <alirezasaadat056@gmail.com>
2024-07-16 05:39:43 -0700

    adding the player

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,8 +1,14 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
-canvas.width = innerWidth
-canvas.height = innerHeight
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let x = canvas.width / 2;
+let y = canvas.height / 2;
+
+ctx.fillStyle = "black"
+ctx.fillRect(0, 0, canvas.width, canvas.height)
 
 class Player {
     constructor(x, y, radius, color) {
@@ -13,8 +19,25 @@ class Player {
     }
     draw() {
         ctx.beginPath();
-        ctx.arc(this.x, this.y, this.radius, 0, Math.PI, false);
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
         ctx.fillStyle = this.color;
-        ctx.fill()
+        ctx.fill();
     }
 }
+
+const player = new Player(x, y, 15, "white");
+
+
+let animationId;
+
+function animate() {
+    animationId = requestAnimationFrame(animate);
+    ctx.fillStyle = 'rgba(0 , 0 , 0 , 0.1)';
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
+
+    player.draw()
+
+
+}
+
+animate()

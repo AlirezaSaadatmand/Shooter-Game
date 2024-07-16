4f347db7f4752e372884b341ea3528a0a3a08ce6
Alireza <alirezasaadat056@gmail.com>
2024-07-16 05:09:48 -0700

    adding the Player class

--- Javascript/script.js
+++ Javascript/script.js
@@ -3,3 +3,18 @@ const ctx = canvas.getContext("2d");
 
 canvas.width = innerWidth
 canvas.height = innerHeight
+
+class Player {
+    constructor(x, y, radius, color) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+    }
+    draw() {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI, false);
+        ctx.fillStyle = this.color;
+        ctx.fill()
+    }
+}
